0a3435070df15e23cd48980f92c929fc42cc4de6
Jakub Chmielwski <kuba91821@gmail.com>
2025-01-23 23:33:15 +0100

    fix: routing to specific note in tags tab on mobile

--- src/pages/mobile/Tags.tsx
+++ src/pages/mobile/Tags.tsx
@@ -73,7 +73,7 @@ const Tags: React.FC = () => {
                             <NotesList
                                 desktopLayout={false}
                                 filteredNotes={filteredNotes}
-                                route="/tag"
+                                route={`/tag/${selectedTag}`}
                             />
 
                             <button
